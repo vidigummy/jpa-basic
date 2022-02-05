e8cf574f1b9054e3e4c0c4e8a3c7848377721046
vidigummy <vidigummy@vidigummyui-MacBookAir.local>
2022-02-05 19:47:47 +0900

    transaction

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -46,17 +46,33 @@ public class JpaMain {
 //                System.out.println("member.name   :   "  + member.getName());
 //            }
 
-//            영속 컨텍스트에 대한 이야기
-            Member member = new Member();
-            member.setName("vidigummy");
-            member.setId(10L);
-//            여기까지는 비영속 상태
-            em.persist(member);
+//            ***********영속 컨텍스트에 대한 이야기***********
+//            Member member = new Member();
+//            member.setName("vidigummy");
+//            member.setId(101L);
+////            여기까지는 비영속 상태
+//            em.persist(member);
+//
+////            여기서부터 영속 상태(DB에는 저장이 안 된 상태이다. commit을 해야 DB에 트랜잭션 상태가 변화한다.)
+////            em.detach(member);//를 하면 영속성 에서 사라진다.(쿼리도 안 나감~)
+//            Member findMember1 = em.find(Member.class, 101L);
+//            System.out.println("findMember1 = " + findMember1.getName());
+//            em.detach(member);
+//            //라고 하면 1차 캐시에서 지워져서 쿼리가 나가지도 않는다. 그리고 find할 때는 1차 캐시에서 찾아서 쿼리를 작성할 필요가 없어진다.!!
+//            // 추가적으로, 어떤 entity를 한 트랙잭션 안에서 찾아놓으면, 다시 쿼리를 날리지 않는다. -> 1차 캐시에 남아있기 때문이다~
 
-//            여기서부터 영속 상태(DB에는 저장이 안 된 상태이다. commit을 해야 DB에 트랜잭션 상태가 변화한다.)
-//            em.detach(member);//를 하면 영속성 에서 사라진다.(쿼리도 안 나감~)
-
-
+//            ***********Entity 쓰기 지연에 대한 이야기~~***********
+//            CREATE 할 경우
+//            Member member1 = new Member(150L, "testA");
+//            Member member2 = new Member(151L, "testB");
+//
+//            em.persist(member1);
+//            em.persist(member2);
+//            //여기까지는 1차 캐시 및 쓰기 저장소에 들어있을 뿐, DB에 올라가진 않는다.
+//            UPDATE의 경우
+//            Member member = em.find(Member.class, 150L);
+//            member.setName("testU2");
+//            em.detach(member);//하면 안바뀌지롱~
 
             tx.commit();
         } catch (Exception e){
